8b3ea56e7f1ed695b8558dbcf6b1db4352f4a4c2
Gold Edem Hogan <edge37@outlookcom>
2024-02-14 18:57:54 -1200

    .

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const message_index="message"
 export const message_id_prefix="message_"
 export const items_per_page=7
+export const embedding_field_name="v"
--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -10,16 +10,18 @@
 	}
 </script>
 
-<h2>All messages</h2>
-<Button href="/messages/add">send a message</Button>
-
-<div class="space-y-4">
-	{#each data._ as m}
-		<div class="grid grid-cols-2 gap-3">
-			<p class="p-4 shadow-md bg-rose-300 rounded-lg items-center" >{m.value.m}</p>
-			{#if data.in}
-				<form method="post" action="?/approve"><Button icon={Check} circle name="id" value={m.id} /></form>
-			{/if}
-		</div>
-{/each}
+<div class="space-y-2">
+	<h2>All messages</h2>
+	<Button href="/messages/add">send a message</Button>
+	
+	<div class="space-y-4">
+		{#each data._ as m}
+			<div class="grid grid-cols-2 gap-3">
+				<p class="p-4 shadow-md bg-rose-300 rounded-lg items-center" >{m.value.m}</p>
+				{#if data.in}
+					<form method="post" action="?/approve"><Button icon={Check} circle name="id" value={m.id} /></form>
+				{/if}
+			</div>
+	{/each}
+	</div>
 </div>
