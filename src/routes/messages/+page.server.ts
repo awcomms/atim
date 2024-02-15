a402309f3b17935849147f482ce649ed87d39940
Gold Edem Hogan <edge37@outlookcom>
2024-02-14 18:32:20 -1200

    .

--- src/routes/messages/+page.server.ts
+++ src/routes/messages/+page.server.ts
@@ -1,9 +1,9 @@
 import { message_index as index } from '$lib/constants';
 import { client } from '$lib/util/redis';
 import { search } from '$lib/util/redis/search';
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load = async ({locals}) => {
+export const load: PageServerLoad = async ({locals}) => {
 	// const res = await client.ft.search(index, '*', {
 	// 	LIMIT: { from: 0, size: items_per_page },
 	// 	RETURN: ['m']
@@ -16,9 +16,7 @@ export const load = async ({locals}) => {
 
 export const actions: Actions = {
     approve: async ({request}) => {
-        console.info('hi')
         const data = await request.formData()
-        console.info('fd', data)
         const id = data.get('id') as string
         await client.json.set(id, '$.a', 1)
     }
--- src/routes/messages/+page.svelte
+++ src/routes/messages/+page.svelte
@@ -9,11 +9,13 @@
 <h2>All messages</h2>
 <Button href="/messages/add">send a message</Button>
 
-{#each data._ as m}
-	<div class="space-y-3">
-		<p class="p-4 shadow-xl bg-rose-300" >{m.value.m}</p>
-		{#if data.in}
-			<form method="post" action="?/approve"><Button icon={Check} circle name="id" value={m.id} /></form>
-		{/if}
-	</div>
+<div class="space-y-4">
+	{#each data._ as m}
+		<div class="grid grid-cols-2 gap-3">
+			<p class="p-4 shadow-xl bg-rose-300 rounded-lg items-center" >{m.value.m}</p>
+			{#if data.in}
+				<form method="post" action="?/approve"><Button icon={Check} circle name="id" value={m.id} /></form>
+			{/if}
+		</div>
 {/each}
+</div>
